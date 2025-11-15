a095f8122968a8ca516bce942a32390767549ae9
PyuKu2005 <allardl2005@gmail.com>
2025-11-14 22:32:57 -0500

    Ajoute de state Held et Free

---
+++ Scenes/Ball/States/ball_state_held.gd
@@ -0,0 +1,19 @@
+extends BallStateMaster
+class_name  BallStateHeld
+#Est appellé lorsqu'on entre dans le state
+
+#Est appellé lorsqu'on entre dans le state
+func enter():
+	super.enter()
+
+#Est appellé lorsqu'on sort du state
+func exit():
+	super.exit()
+
+#Est appellé lorsqu'un input est détecté
+func handle_input(input: String, value: bool):
+	super.handle_input(input,value)
+	
+#Est appellé à chaque frame
+func update():
+	super.update()
