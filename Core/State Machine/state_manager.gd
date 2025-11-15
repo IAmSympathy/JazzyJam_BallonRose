1adf71100ecd5c41c49396d08e48faee964a0cc8
IAmSympathy <teamshadows07@gmail.com>
2025-11-14 22:31:13 -0500

    Ajoute la possibilité de listé les states dans le StateManager

--- Core/State Machine/state_manager.gd
+++ Core/State Machine/state_manager.gd
@@ -3,21 +3,22 @@ class_name StateManager
 
 @export var active_state: State
 @export var possessed_node : Node2D
+@export var states: Dictionary[String, State]
 
 func _ready() -> void:
 	active_state.enter()
 
-#À appeller pour changer de state
-func handle_state_transition(next_state: State):
+# À appeller pour changer de state
+func handle_state_transition(next_state_name: String):
 	active_state.exit()
-	active_state = next_state
+	active_state = states[next_state_name]
 	active_state.enter()
 
-#À appeller dans le possessed pawn lorsqu'un input est détecté
+# À appeller dans le possessed pawn lorsqu'un input est détecté
 func handle_state_input(input : String, value : bool):
 	active_state.handle_input(input, value)	
 
-#À appeller dans le _process du possessed pawn
+# À appeller dans le _process du possessed pawn
 func call_active_state_update():
 	active_state.update()
 	
