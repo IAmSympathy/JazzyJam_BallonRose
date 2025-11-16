8b88940075421b7ad6e862c721afda86545327c0
IAmSympathy <teamshadows07@gmail.com>
2025-11-16 08:04:45 -0500

    hide level start in game

--- Levels/Common/level_master.gd
+++ Levels/Common/level_master.gd
@@ -4,6 +4,9 @@ class_name LevelMaster
 @export var level_name: String = "Default Level Name"
 signal end_reached
 
+func _ready() -> void:
+	$Start.visible = false
+
 func _on_end_body_entered(body: Node2D) -> void:
 	end_reached.emit()
 	print("Level Complete!")
